ed4cb0626e45ee3e4835cd3bb8d4ee76cc266fc2
TanjilulAnwar <tanjilulanwar@gmail.com>
2021-11-22 16:46:55 +0600

    gingham added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="head-text">Hello! I'm Tanjilul Anwar</div>
+
+      <div className="main">
+        <div className="main-item gingham-green">
+          <div className="child-item">
+            <table className="table-green">
+              <tr>
+                <td>
+                  <div className="round-item gingham-green"></div>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="main-item gingham-red">
+          <div className="child-item"></div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="main-item gingham-orange">
+          <div className="child-item"></div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="main-item gingham-purple">
+          <div className="child-item"></div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="main-item gingham-blue">
+          <div className="child-item"></div>
+        </div>
+      </div>
     </div>
   );
 }
